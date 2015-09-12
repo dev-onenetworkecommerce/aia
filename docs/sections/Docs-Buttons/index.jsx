907f3498304ab27aa7a8465cf4b8d1b4f17d85ee
srph <seraphipod@gmail.com>
2015-09-12 15:06:27 +0800

    Remove copy-pasta code from docs.

--- docs/sections/Docs-Buttons/index.jsx
+++ docs/sections/Docs-Buttons/index.jsx
@@ -3,13 +3,7 @@ import React from 'react';
 export default class Buttons extends React.Component {
   render() {
     return (
-      <div className="row">
-        <div className="col-md-3">
-        </div>
-
-        <div className="col-md-9">
-          {this.props.children}
-        </div>
+      <div>
       </div>
     );
   }
--- docs/sections/Docs-Colors/index.jsx
+++ docs/sections/Docs-Colors/index.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
 
 export default class Colors extends React.Component {
-  render() {
+    render() {
     return (
-      <div className="row">
-        <div className="col-md-3">
-        </div>
-
-        <div className="col-md-9">
-          {this.props.children}
-        </div>
+      <div>
       </div>
     );
   }
