a0deb2d90415f59cadfa08a5e300a3d6a1f79761
juztinlazaro <juztinlazaro>
2015-09-12 06:34:47 -0400

    add bordered and crimson

--- docs/sections/Docs-Labels/index.jsx
+++ docs/sections/Docs-Labels/index.jsx
@@ -4,6 +4,13 @@ export default class Labels extends React.Component {
   render() {
     return (
       <div>
+         <label className="label -bordered"> 
+         	Default 
+         </label>
+
+         <label className="label -crimson"> 
+         	Danger 
+         </label>
       </div>
     );
   }
