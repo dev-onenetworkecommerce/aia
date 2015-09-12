c1c764e780da061cf5affa1a04621d9fe67c4b22
vistajess <vistajesus8@gmail.com>
2015-09-13 02:37:03 +0800

    fix code review issues

--- docs/sections/Docs-Buttons/index.jsx
+++ docs/sections/Docs-Buttons/index.jsx
@@ -12,15 +12,6 @@ export default class Buttons extends React.Component {
           <button className="btn -warning">Button</button>
           <button className="btn -danger">Button</button>
         </div>
-        <h1>Hovered / Darker Buttons</h1>
-        <div className="doc-bottom-space">
-          <button className="btn" disabled>Button</button>
-          <button className="btn -info-dk">Button</button>
-          <button className="btn -success-dk">Button</button>
-          <button className="btn -warning-dk">Button</button>
-          <button className="btn -danger-dk">Button</button>
-        </div>
-
         <h1>Sizes</h1>
         <div className="doc-bottom-space">
           <button className="btn -extra-small -warning" >Extra small Button</button>
@@ -29,11 +20,11 @@ export default class Buttons extends React.Component {
         </div>
         <h1>Button Group</h1>
         <div className="doc-bottom-space">
-          <span className="btn-group">
+          <div className="btn-group">
             <button className="btn -warning">Button</button>
             <button className="btn -info">Button</button>
             <button className="btn -success">Button</button>
-          </span>
+          </div>
         </div>
 
       </div>
