57f52118b8523ec7283d48561c8732edf55957ef
srph <seraphipod@gmail.com>
2015-09-13 03:26:38 +0800

    Improve panel and tables.
    Improve css; fixes.
    Improve doc presentation.

--- docs/sections/Docs-Panels/index.jsx
+++ docs/sections/Docs-Panels/index.jsx
@@ -4,94 +4,100 @@ export default class Panels extends React.Component {
   render() {
     return (
     	<div>
-    		<h2>Basic Panel</h2>
-    		<div className="panel -default">
-    			<div className="body">Basic Panel Example</div>
-    		</div>
+        <section className="doc-bottom-space-large">
+      		<h2>Basic Panel</h2>
+      		<div className="panel -default">
+      			<div className="body">Basic Panel Example</div>
+      		</div>
+        </section>
 
-    		<h2>Panel with Header and Body</h2>
-	      	<div className="panel -default">
-	      		<div className="heading">Panel Title</div>
-	      		<div className="body">Panel Body</div>
-	      	</div>
+        <section className="doc-bottom-space-large">
+      		<h2>Panel with Header and Body</h2>
+        	<div className="panel -default">
+        		<div className="heading">Panel Title</div>
+        		<div className="body">Panel Body</div>
+        	</div>
+        </section>
 
-	      	<h2>Panel with Footer and Body </h2>
-	      	<div className="panel -default">
-	      		<div className="body">Panel Body</div>
-	      		<div className="footer">Panel Footer</div>
-	      	</div>
+        <section className="doc-bottom-space-large">
+        	<h2>Panel with Footer and Body </h2>
+        	<div className="panel -default">
+        		<div className="body">Panel Body</div>
+        		<div className="footer">Panel Footer</div>
+        	</div>
+        </section>
 
-	      	<h2>Contextual Alternatives</h2>
-	      	<div className="panel -sky">
-	      		<div className="heading">Panel Title</div>
-	      		<div className="body">Panel Body</div>
-	      	</div>
+        <section className="doc-bottom-space-large">
+        	<h2>Contextual Alternatives</h2>
+        	<div className="panel -sky doc-bottom-space">
+        		<div className="heading">Panel Title</div>
+        		<div className="body">Panel Body</div>
+        	</div>
 
-	      	<div className="panel -emerald">
-	      		<div className="heading">Panel Title</div>
-	      		<div className="body">Panel Body</div>
-	      	</div>
+        	<div className="panel -emerald doc-bottom-space">
+        		<div className="heading">Panel Title</div>
+        		<div className="body">Panel Body</div>
+        	</div>
 
-	      	<div className="panel -sun">
-	      		<div className="heading">Panel Title</div>
-	      		<div className="body">Panel Body</div>
-	      	</div>
+        	<div className="panel -sun doc-bottom-space">
+        		<div className="heading">Panel Title</div>
+        		<div className="body">Panel Body</div>
+        	</div>
 
-	      	<div className="panel -crimson">
-	      		<div className="heading">Panel Title</div>
-	      		<div className="body">Panel Body</div>
-	      	</div>
+        	<div className="panel -crimson">
+        		<div className="heading">Panel Title</div>
+        		<div className="body">Panel Body</div>
+        	</div>
+        </section>
 
-	      	<h2>Panel with Tables</h2>
-	      	<div className="panel -default">
-    			<div className="heading">Panel Title</div>
-    			<div className="body">
-    				Some default panel content here. Nulla vitae elit libero, a pharetra augue. Aenean lacinia bibendum nulla sed
-					consectetur. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nullam id
-					dolor id nibh ultricies vehicula ut id eli
-				</div>
-				<div className="table-responsive">
-					<table>
-						<thead>
-							<tr>
-								<th>#</th>
-								<th>Table Header</th>
-								<th>Table Header</th>
-								<th>Table Header</th>
-							</tr>
-						</thead>
-						<tbody>
-							<tr>
-								<td>1</td>
-								<td>Table Content</td>
-								<td>Table Content</td>
-								<td>Table Content</td>
-							</tr>
-							<tr>
-								<td>2</td>
-								<td>Table Content</td>
-								<td>Table Content</td>
-								<td>Table Content</td>
-							</tr>
-						</tbody>
-					</table>
-				</div>
-    		</div>
+        <section className="doc-bottom-space-large">
+        	<h2>Panel with Tables</h2>
+        	<div className="panel -default">
+            <div className="heading">Panel Title</div>
+            <div className="body">
+      				Some default panel content here. Nulla vitae elit libero, a pharetra augue. Aenean lacinia bibendum nulla sed consectetur. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nullam id dolor id nibh ultricies vehicula ut id eli.
+            </div>
+    				<table className="table">
+    					<thead>
+    						<tr>
+    							<th>#</th>
+    							<th>Table Header</th>
+    							<th>Table Header</th>
+    							<th>Table Header</th>
+    						</tr>
+    					</thead>
+    					<tbody>
+    						<tr>
+    							<td>1</td>
+    							<td>Table Content</td>
+    							<td>Table Content</td>
+    							<td>Table Content</td>
+    						</tr>
+    						<tr>
+    							<td>2</td>
+    							<td>Table Content</td>
+    							<td>Table Content</td>
+    							<td>Table Content</td>
+    						</tr>
+    					</tbody>
+            </table>
+  				</div>
+        </section>
 
-    		<h2>Panel With List Groups</h2>
-    		<div className="panel -default">
-    			<div className="heading">Panel Title</div>
-    			<div className="body">
-    				Some default panel content here. Nulla vitae elit libero, a pharetra augue. Aenean lacinia bibendum nulla sed
-					consectetur. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nullam id
-					dolor id nibh ultricies vehicula ut id eli
-    			</div>
-	    		<ul className="list-group">
-	    			<li className="menuitem">List Item</li>
-	    			<li className="menuitem">List Item</li>
-	    			<li className="menuitem">List Item</li>
-	    		</ul>
-    		</div>
+        <section>
+      		<h2>Panel With List Groups</h2>
+      		<div className="panel -default">
+            <div className="heading">Panel Title</div>
+            <div className="body">
+              Some default panel content here. Nulla vitae elit libero, a pharetra augue. Aenean lacinia bibendum nulla sed consectetur. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nullam id dolor id nibh ultricies vehicula ut id eli
+            </div>
+        		<div className="list-group">
+        			<a className="menuitem">List Item</a>
+        			<a className="menuitem">List Item</a>
+        			<a className="menuitem">List Item</a>
+        		</div>
+          </div>
+        </section>
 	    </div>
     );
   }
--- docs/sections/Docs-Tables/index.jsx
+++ docs/sections/Docs-Tables/index.jsx
@@ -3,192 +3,192 @@ import React from 'react';
 export default class Tables extends React.Component {
   render() {
     return (
-    	<div>
-    		<h2>Basic Table</h2>
-    		<div className="table-responsive">
-		      	<table>
-					<thead>
-						<tr>
-							<th>#</th>
-							<th>Table Header</th>
-							<th>Table Header</th>
-							<th>Table Header</th>
-						</tr>
-					</thead>
+      <div>
+        <section className="doc-bottom-space-large">
+          <h2>Basic Table</h2>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Table Header</th>
+                <th>Table Header</th>
+                <th>Table Header</th>
+              </tr>
+            </thead>
 
-					<tbody>
-						<tr>
-							<td>1</td>
-							<td>Table Content</td>
-							<td>Table Content</td>
-							<td>Table Content</td>
-						</tr>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Table Content</td>
+                <td>Table Content</td>
+                <td>Table Content</td>
+              </tr>
 
-						<tr>
-							<td>2</td>
-							<td>Table Content</td>
-							<td>Table Content</td>
-							<td>Table Content</td>
-						</tr>
-					</tbody>
-				</table>
-			</div>
+              <tr>
+                <td>2</td>
+                <td>Table Content</td>
+                <td>Table Content</td>
+                <td>Table Content</td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
 
-			<h2>Striped Rows</h2>
-			<div className="table-responsive">
-				<table className="-striped">
-					<thead>
-						<tr>
-							<th>#</th>
-							<th>Table Header</th>
-							<th>Table Header</th>
-							<th>Table Header</th>
-						</tr>
-					</thead>
+        <section className="doc-bottom-space-large">
+          <h2>Striped Rows</h2>
+          <table className="table -striped">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Table Header</th>
+                <th>Table Header</th>
+                <th>Table Header</th>
+              </tr>
+            </thead>
 
-					<tbody>
-						<tr>
-							<td>1</td>
-							<td>Table Content</td>
-							<td>Table Content</td>
-							<td>Table Content</td>
-						</tr>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Table Content</td>
+                <td>Table Content</td>
+                <td>Table Content</td>
+              </tr>
 
-						<tr>
-							<td>2</td>
-							<td>Table Content</td>
-							<td>Table Content</td>
-							<td>Table Content</td>
-						</tr>
+              <tr>
+                <td>2</td>
+                <td>Table Content</td>
+                <td>Table Content</td>
+                <td>Table Content</td>
+              </tr>
 
-						<tr>
-							<td>3</td>
-							<td>Table Content</td>
-							<td>Table Content</td>
-							<td>Table Content</td>
-						</tr>
-					</tbody>
-				</table>
-			</div>
+              <tr>
+                <td>3</td>
+                <td>Table Content</td>
+                <td>Table Content</td>
+                <td>Table Content</td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
 
-			<h2>Bordered Table</h2>
-			<div className="table-responsive">
-				<table className="-bordered">
-					<thead>
-						<tr>
-							<th>#</th>
-							<th>Table Header</th>
-							<th>Table Header</th>
-							<th>Table Header</th>
-						</tr>
-					</thead>
+        <section className="doc-bottom-space-large">
+        <h2>Bordered Table</h2>
+        <table className="table -bordered">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Table Header</th>
+              <th>Table Header</th>
+              <th>Table Header</th>
+            </tr>
+          </thead>
 
-					<tbody>
-						<tr>
-							<td>1</td>
-							<td>Table Content</td>
-							<td>Table Content</td>
-							<td>Table Content</td>
-						</tr>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Table Content</td>
+              <td>Table Content</td>
+              <td>Table Content</td>
+            </tr>
 
-						<tr>
-							<td>2</td>
-							<td>Table Content</td>
-							<td>Table Content</td>
-							<td>Table Content</td>
-						</tr>
+            <tr>
+              <td>2</td>
+              <td>Table Content</td>
+              <td>Table Content</td>
+              <td>Table Content</td>
+            </tr>
 
-						<tr>
-							<td>3</td>
-							<td>Table Content</td>
-							<td>Table Content</td>
-							<td>Table Content</td>
-						</tr>
-					</tbody>
-				</table>
-			</div>
+            <tr>
+              <td>3</td>
+              <td>Table Content</td>
+              <td>Table Content</td>
+              <td>Table Content</td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
 
-			<h2>Hovered Table</h2>
-			<div className="table-responsive">
-				<table>
-					<thead>
-						<tr>
-							<th>#</th>
-							<th>Table Header</th>
-							<th>Table Header</th>
-							<th>Table Header</th>
-						</tr>
-					</thead>
+      <section className="doc-bottom-space-large">
+        <h2>Hovered Table</h2>
+        <table className="table -hovered">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Table Header</th>
+              <th>Table Header</th>
+              <th>Table Header</th>
+            </tr>
+          </thead>
 
-					<tbody>
-						<tr className="-link">
-							<td>1</td>
-							<td>Table Content</td>
-							<td>Table Content</td>
-							<td>Table Content</td>
-						</tr>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Table Content</td>
+              <td>Table Content</td>
+              <td>Table Content</td>
+            </tr>
 
-						<tr className="-link">
-							<td>2</td>
-							<td>Table Content</td>
-							<td>Table Content</td>
-							<td>Table Content</td>
-						</tr>
+            <tr>
+              <td>2</td>
+              <td>Table Content</td>
+              <td>Table Content</td>
+              <td>Table Content</td>
+            </tr>
 
-						<tr className="-link">
-							<td>3</td>
-							<td>Table Content</td>
-							<td>Table Content</td>
-							<td>Table Content</td>
-						</tr>
-					</tbody>
-				</table>
-			</div>
+            <tr>
+              <td>3</td>
+              <td>Table Content</td>
+              <td>Table Content</td>
+              <td>Table Content</td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
 
-			<h2>Contextual Classes</h2>
-			<div className="table-responsive">
-				<table>
-					<thead>
-						<tr>
-							<th>#</th>
-							<th>Table Header</th>
-							<th>Table Header</th>
-							<th>Table Header</th>
-						</tr>
-					</thead>
+      <section>
+        <h2>Contextual Classes</h2>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Table Header</th>
+              <th>Table Header</th>
+              <th>Table Header</th>
+            </tr>
+          </thead>
 
-					<tbody>
-						<tr className="-emerald">
-							<td>1</td>
-							<td>Table Content</td>
-							<td>Table Content</td>
-							<td>Table Content</td>
-						</tr>
+          <tbody>
+            <tr className="-emerald">
+              <td>1</td>
+              <td>Table Content</td>
+              <td>Table Content</td>
+              <td>Table Content</td>
+            </tr>
 
-						<tr className="-sun">
-							<td>2</td>
-							<td>Table Content</td>
-							<td>Table Content</td>
-							<td>Table Content</td>
-						</tr>
+            <tr className="-sun">
+              <td>2</td>
+              <td>Table Content</td>
+              <td>Table Content</td>
+              <td>Table Content</td>
+            </tr>
 
-						<tr className="-crimson">
-							<td>3</td>
-							<td>Table Content</td>
-							<td>Table Content</td>
-							<td>Table Content</td>
-						</tr>
+            <tr className="-crimson">
+              <td>3</td>
+              <td>Table Content</td>
+              <td>Table Content</td>
+              <td>Table Content</td>
+            </tr>
 
-						<tr className="-sky">
-							<td>4</td>
-							<td>Table Content</td>
-							<td>Table Content</td>
-							<td>Table Content</td>
-						</tr>
-					</tbody>
-				</table>
-			</div>
-		</div>
+            <tr className="-sky">
+              <td>4</td>
+              <td>Table Content</td>
+              <td>Table Content</td>
+              <td>Table Content</td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
+    </div>
     );
   }
 }
