79bddb894bb9521fcb89b639a4d05d93964322e6
maebe22 <maeby1115@gmail.com>
2015-09-12 22:18:03 +0800

    ready for inspection

--- docs/sections/Docs-Tables/index.jsx
+++ docs/sections/Docs-Tables/index.jsx
@@ -5,62 +5,189 @@ export default class Tables extends React.Component {
     return (
     	<div>
     		<h2>Basic Table</h2>
-	      	<table className="table">
-				<thead>
-					<tr>
-						<th>#</th>
-						<th>Table Header</th>
-						<th>Table Header</th>
-						<th>Table Header</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td>1</td>
-						<td>Table Content</td>
-						<td>Table Content</td>
-						<td>Table Content</td>
-					</tr>
-					<tr>
-						<td>2</td>
-						<td>Table Content</td>
-						<td>Table Content</td>
-						<td>Table Content</td>
-					</tr>
-				</tbody>
-			</table>
+    		<div className="table-responsive">
+		      	<table>
+					<thead>
+						<tr>
+							<th>#</th>
+							<th>Table Header</th>
+							<th>Table Header</th>
+							<th>Table Header</th>
+						</tr>
+					</thead>
 
-			<h2>Striped Table</h2>
-			<table className="table -striped">
-				<thead>
-					<tr>
-						<th>#</th>
-						<th>Table Header</th>
-						<th>Table Header</th>
-						<th>Table Header</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td>1</td>
-						<td>Table Content</td>
-						<td>Table Content</td>
-						<td>Table Content</td>
-					</tr>
-					<tr>
-						<td>2</td>
-						<td>Table Content</td>
-						<td>Table Content</td>
-						<td>Table Content</td>
-					</tr>
-					<tr>
-						<td>3</td>
-						<td>Table Content</td>
-						<td>Table Content</td>
-						<td>Table Content</td>
-					</tr>
-				</tbody>
-			</table>
+					<tbody>
+						<tr>
+							<td>1</td>
+							<td>Table Content</td>
+							<td>Table Content</td>
+							<td>Table Content</td>
+						</tr>
+
+						<tr>
+							<td>2</td>
+							<td>Table Content</td>
+							<td>Table Content</td>
+							<td>Table Content</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+
+			<h2>Striped Rows</h2>
+			<div className="table-responsive">
+				<table className="-striped">
+					<thead>
+						<tr>
+							<th>#</th>
+							<th>Table Header</th>
+							<th>Table Header</th>
+							<th>Table Header</th>
+						</tr>
+					</thead>
+
+					<tbody>
+						<tr>
+							<td>1</td>
+							<td>Table Content</td>
+							<td>Table Content</td>
+							<td>Table Content</td>
+						</tr>
+
+						<tr>
+							<td>2</td>
+							<td>Table Content</td>
+							<td>Table Content</td>
+							<td>Table Content</td>
+						</tr>
+
+						<tr>
+							<td>3</td>
+							<td>Table Content</td>
+							<td>Table Content</td>
+							<td>Table Content</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+
+			<h2>Bordered Table</h2>
+			<div className="table-responsive">
+				<table className="-bordered">
+					<thead>
+						<tr>
+							<th>#</th>
+							<th>Table Header</th>
+							<th>Table Header</th>
+							<th>Table Header</th>
+						</tr>
+					</thead>
+
+					<tbody>
+						<tr>
+							<td>1</td>
+							<td>Table Content</td>
+							<td>Table Content</td>
+							<td>Table Content</td>
+						</tr>
+
+						<tr>
+							<td>2</td>
+							<td>Table Content</td>
+							<td>Table Content</td>
+							<td>Table Content</td>
+						</tr>
+
+						<tr>
+							<td>3</td>
+							<td>Table Content</td>
+							<td>Table Content</td>
+							<td>Table Content</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+
+			<h2>Hovered Table</h2>
+			<div className="table-responsive">
+				<table>
+					<thead>
+						<tr>
+							<th>#</th>
+							<th>Table Header</th>
+							<th>Table Header</th>
+							<th>Table Header</th>
+						</tr>
+					</thead>
+
+					<tbody>
+						<tr className="-link">
+							<td>1</td>
+							<td>Table Content</td>
+							<td>Table Content</td>
+							<td>Table Content</td>
+						</tr>
+
+						<tr className="-link">
+							<td>2</td>
+							<td>Table Content</td>
+							<td>Table Content</td>
+							<td>Table Content</td>
+						</tr>
+
+						<tr className="-link">
+							<td>3</td>
+							<td>Table Content</td>
+							<td>Table Content</td>
+							<td>Table Content</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+
+			<h2>Contextual Classes</h2>
+			<div className="table-responsive">
+				<table>
+					<thead>
+						<tr>
+							<th>#</th>
+							<th>Table Header</th>
+							<th>Table Header</th>
+							<th>Table Header</th>
+						</tr>
+					</thead>
+
+					<tbody>
+						<tr className="-emerald">
+							<td>1</td>
+							<td>Table Content</td>
+							<td>Table Content</td>
+							<td>Table Content</td>
+						</tr>
+
+						<tr className="-sun">
+							<td>2</td>
+							<td>Table Content</td>
+							<td>Table Content</td>
+							<td>Table Content</td>
+						</tr>
+
+						<tr className="-crimson">
+							<td>3</td>
+							<td>Table Content</td>
+							<td>Table Content</td>
+							<td>Table Content</td>
+						</tr>
+
+						<tr className="-sky">
+							<td>4</td>
+							<td>Table Content</td>
+							<td>Table Content</td>
+							<td>Table Content</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
 		</div>
     );
   }
