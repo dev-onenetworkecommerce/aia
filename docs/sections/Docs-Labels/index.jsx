15fcb3379cd943379980a42f31a3c51e9d0190f2
juztinlazaro <juztinlazaro>
2015-09-12 10:36:21 -0400

    fix issues

--- docs/sections/Docs-Labels/index.jsx
+++ docs/sections/Docs-Labels/index.jsx
@@ -4,97 +4,101 @@ export default class Labels extends React.Component {
   render() {
     return (
       <div>
-      	 <h2 className="title">
-      	 	Labels & Badges
-      	 </h2>
-      	 <p>
-      	 	This is a collection of symbols for quickly mocking up web applications, created by your friends at One Network Ecommerce and released under the ONE license.	
-      	 </p>
+	      	 <h2 className="title">
+	      	 	Labels & Badges
+	      	 </h2>
+	      	 <p>
+	      	 	This is a collection of symbols for quickly mocking up web applications, created by your friends at One Network Ecommerce and released under the ONE license.	
+	      	 </p>
 
-        <div className="col-md-6">
+	        <div className="col-md-6">
 
-          	<h4>Example</h4>
+	          	<h4>Example</h4>
 
-          	<h4>Example Heading</h4>
+	          	<h4>Example Heading</h4>
 
-          	<h4>Example Heading</h4>
+	          	<h4>Example Heading</h4>
 
-          	<h4>Example Heading</h4>
+	          	<h4>Example Heading</h4>
 
-          	<h4>Example Heading</h4>
+	          	<h4>Example Heading</h4>
 
-          	<h4>Example Heading</h4>
+	          	<h4>Example Heading</h4>
 
-          	<h4>Example Heading</h4>
+	          	<h4>Example Heading</h4>
 
-          	<h4>Example Heading</h4>
+	          	<h4>Example Heading</h4>
 
-        </div>
-
-        <div className="col-md-6 variations-label">
-        	 <div className="col-md-12">
-	        	 <h3>
-	        	 	Available variations
-	        	 </h3>
-
-		         <label className="label -bordered"> 
-		         	Default 
-		         </label>
-
-		         <label className="label -primary"> 
-		         	Primary 
-		         </label>
-
-		         <label className="label -emerald"> 
-		         	Success 
-		         </label>
-
-		         <label className="label -sky"> 
-		         	Info 
-		         </label>
-
-		         <label className="label -sun"> 
-		         	Warning 
-		         </label>
-
-		         <label className="label -crimson"> 
-		         	Danger 
-		         </label>
-
-			   <div className="add-margin-40x">
-				    <h3>Badges</h3>
-
-			        News Feed  <span className="badges -bordered">20</span>
-
-			        <br/>
-
-			        News Feed <span className="badges -primary">40</span>
-
-			        <br/>
-
-			        News Feed  <span className="badges -emerald">50</span>
-
-			        <br/>
-
-			        News Feed  <span className="badges -sky">60</span>
-
-			        <br/>
-
-			        News Feed <span className="badges -sun">70</span>
-
-			        <br/>
-
-			        News Feed <span className="badges -crimson">80</span>
-			   </div>
 	        </div>
-	    	<div className="col-md-12 add-margin-40x">
-	    		<span className="status -active"></span> Active
-	    		<span className="status -inactive"></span> Inactive
-	    	</div>
-        </div>
 
-	          
+	        <div className="col-md-6 variations-label">
+	        	 <div className="col-md-12">
+		        	 <h3>
+		        	 	Available variations
+		        	 </h3>
+
+			         <span className="label -bordered"> 
+			         	Default 
+			         </span>
+
+			         <span className="label -primary"> 
+			         	Primary 
+			         </span>
+
+			         <span className="label -emerald"> 
+			         	Success 
+			         </span>
+
+			         <span className="label -sky"> 
+			         	Info 
+			         </span>
+
+			         <span className="label -sun"> 
+			         	Warning 
+			         </span>
+
+			         <span className="label -crimson"> 
+			         	Danger 
+			         </span>
+
+				   <div className="add-margin-40x">
+					    <h3>Badges</h3>
+
+				       	<div>
+				       	  News Feed  <span className="badges -bordered">20</span>
+				       	</div>
+
+				        <div>
+				          News Feed <span className="badges -primary">40</span>
+				        </div>
+				      
+				        <div>
+				          News Feed  <span className="badges -emerald">50</span>
+				        </div>
+
+				        <div>
+				          News Feed  <span className="badges -sky">60</span>
+				        </div>
+
+				       <div>
+				       	   News Feed <span className="badges -sun">70</span>
+				       </div>
+
+					   <div>
+					   	   News Feed <span className="badges -crimson">80</span>
+					   </div>
+				   </div>
+		        </div>
+
+		    	<div className="col-md-12 add-margin-40x">
+		    		<span className="status -active"></span> Active
+
+		    		<span className="status -inactive"></span> Inactive
+		    	</div>
+
         </div>
+	          
+   </div>
 
     );
   }
