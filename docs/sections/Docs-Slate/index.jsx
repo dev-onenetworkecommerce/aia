4c7dbd11bdf7057398dac878855db713ddaa969d
vistajess <vistajesus8@gmail.com>
2015-09-12 19:36:27 +0800

    Added hyphen on variation classes

--- docs/sections/Docs-Slate/index.jsx
+++ docs/sections/Docs-Slate/index.jsx
@@ -8,15 +8,15 @@ export default class Slate extends React.Component {
         	<h2>Simple Slate</h2>
         	<small>simple slate with padding 30px on all sides and 10px margin on all sides.</small>
         </div>
-        <div className="slate spacious">
+        <div className="slate -spacious">
           <h2>Slate spacious</h2>
           <small>Adds padding to your contents</small>
         </div>
-        <div className="slate tidy-background">
+        <div className="slate -tidy-background">
           <h2>Slate with No Background</h2>
           <small>No border-radius and white background</small>
         </div>
-        <div className="slate half-slate">
+        <div className="slate -half-slate">
           <h2>Half Slate</h2>
           <small>Slate with 50% width</small>
         </div>
