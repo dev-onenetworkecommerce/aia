ba2511f46c73cd82521fb27486962779c06ea8a6
srph <seraphipod@gmail.com>
2015-09-12 16:17:11 +0800

    Init `button` sec content.

--- docs/sections/Docs-Buttons/index.jsx
+++ docs/sections/Docs-Buttons/index.jsx
@@ -4,6 +4,8 @@ export default class Buttons extends React.Component {
   render() {
     return (
       <div>
+        <h1>Buttons</h1>
+        <button className="btn">Button</button>
       </div>
     );
   }
