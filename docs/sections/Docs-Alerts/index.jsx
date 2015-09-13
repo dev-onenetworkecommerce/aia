0d73ed23ffd668869b9ad70c33e314d948dec269
srph <seraphipod@gmail.com>
2015-09-13 20:28:28 +0800

    Bulk update.
    Add utils page.
    Fix alert variant terms.
    Add -large modifier for alert.

--- docs/sections/Docs-Alerts/index.jsx
+++ docs/sections/Docs-Alerts/index.jsx
@@ -4,26 +4,44 @@ export default class Alerts extends React.Component {
   render() {
     return (
       <div>
-        <h1 className="doc-heading">Alerts</h1>
-      	<p className="lead">
-          This is a collection of symbols for quickly mocking up web applications. Created by your friends at One Network Ecommerce and released under the ONE License
-        </p>
+        <section className="doc-bottom-space-large">
+          <h1 className="doc-heading">Alerts</h1>
+        	<p className="lead">
+            This is a collection of symbols for quickly mocking up web applications. Created by your friends at One Network Ecommerce and released under the ONE License
+          </p>
 
-      	<div className="alert -success doc-bottom-space">
-      		Well done! You have successfully read this important alert message.
-      	</div>
+          <div className="alert -sky doc-bottom-space">
+            Heads up! This alert needs your attention, but its not super important.
+          </div>
 
-      	<div className="alert -info doc-bottom-space">
-      		Heads up! This alert needs your attention, but its not super important.
-      	</div>
+        	<div className="alert -emerald doc-bottom-space">
+        		Well done! You have successfully read this important alert message.
+        	</div>
 
-      	<div className="alert -warning doc-bottom-space">
-      		Warning! Better check yourself, youre not looking too good.
-      	</div>
+        	<div className="alert -sun doc-bottom-space">
+        		Warning! Better check yourself, youre not looking too good.
+        	</div>
 
-      	<div className="alert -danger doc-bottom-space">
-      		Oh snap! Change a few things up and try this submitting again.
-      	</div>
+        	<div className="alert -crimson">
+        		Oh snap! Change a few things up and try this submitting again.
+        	</div>
+        </section>
+
+        <section className="doc-bottom-space-large">
+          <h3 className="doc-heading">Example: Block</h3>
+          <p>Useful when you'd like to use the whole space without the border and radius. Simply apply the .-block class.</p>
+          <div className="alert -emerald -block">
+            Oh snap! Change a few things up and try this submitting again.
+          </div>
+        </section>
+
+        <section>
+          <h3 className="doc-heading">Example: Large</h3>
+          <p>Simply apply the .-large class.</p>
+          <div className="alert -emerald -large">
+            Oh snap! Change a few things up and try this submitting again.
+          </div>
+        </section>
       </div>
     );
   }
--- docs/sections/Docs-Utilities/index.jsx
+++ docs/sections/Docs-Utilities/index.jsx
@@ -4,6 +4,34 @@ export default class Utilities extends React.Component {
   render() {
     return (
       <div>
+        <h1 className="doc-heading">Helper classes</h1>
+        <p className="lead">Utility classes are available for usage. <strong>But take note:</strong> Use them sparingly. </p>
+
+        <section className="doc-bottom-space-large">
+          <h3 className="doc-heading">Contextual classes</h3>
+          <p>Convey meaning through color with a handful of emphasis utility classes. These may also be applied to links and will darken on hover just like our default link styles.</p>
+          <p className="text-muted">Fusce dapibus, tellus ac cursus commodo, tortor mauris nibh.</p>
+          <p className="text-emerald">Fusce dapibus, tellus ac cursus commodo, tortor mauris nibh.</p>
+          <p className="text-sky">Fusce dapibus, tellus ac cursus commodo, tortor mauris nibh.</p>
+          <p className="text-sun">Fusce dapibus, tellus ac cursus commodo, tortor mauris nibh.</p>
+          <p className="text-crimson">Fusce dapibus, tellus ac cursus commodo, tortor mauris nibh.</p>
+        </section>
+
+        <section className="doc-bottom-space-large">
+          <section className="doc-bottom-space">
+            <h3 className="doc-heading">Quick floats</h3>
+            <p>Use these to float an element to left or right.</p>
+            <div className="_clearfix">
+              <div className="_left doc-block">._left</div>
+              <div className="_right doc-block">._right</div>
+            </div>
+          </section>
+
+          <section>
+            <h3 className="doc-heading">Clearfix</h3>
+            <p>Simply apply ._clearfix to parents elements to easily clear floats.</p>
+          </section>
+        </section>
       </div>
     );
   }
