f965e389eb4bd73cd78141507563c79cf15fe479
maebe22 <maeby1115@gmail.com>
2015-09-13 10:25:19 +0800

    added normal and disabled button

--- docs/sections/Docs-Buttons/index.jsx
+++ docs/sections/Docs-Buttons/index.jsx
@@ -4,8 +4,24 @@ export default class Buttons extends React.Component {
   render() {
     return (
       <div>
-        <h1>Buttons</h1>
-        <button className="btn">Button</button>
+        <section className="doc-bottom-space-large">
+          <h2>Light Background</h2>
+            <div className="doc-bottom-space">
+              <h4>Normal</h4>          
+              <button className="btn">Button</button>
+            </div>
+
+            <div className="doc-bottom-space">            
+              <h4>Disabled</h4>
+              <button className="btn -disabled">Disabled</button>
+            </div>
+        </section>
+
+        <section className="doc-bottom-space-large">
+          <h2>Button Group</h2>
+          <div>
+          </div>
+        </section>
       </div>
     );
   }
