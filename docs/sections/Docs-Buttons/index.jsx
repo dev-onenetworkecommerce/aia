1b17e06bb3d7342b90e8431a3394aa9e35238977
maebe22 <maeby1115@gmail.com>
2015-09-13 15:01:14 +0800

    continued button

--- docs/sections/Docs-Buttons/index.jsx
+++ docs/sections/Docs-Buttons/index.jsx
@@ -3,10 +3,21 @@ import React from 'react';
 export default class Buttons extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Buttons</h1>
-        <button className="btn">Button</button>
-      </div>
+      <section className="doc-bottom-space-large">
+        <div className="doc-bottom-space">
+          <h2>Standard Button</h2>
+          <button className="btn -default">Button</button>
+        </div>
+
+        <div className="doc-bottom-space">
+          <h2>Emphasis</h2>
+          <button className="btn -sky">Button</button>
+          <button className="btn -sun">Button</button>
+          <button className="btn -crimson">Button</button>
+          <button className="btn -emerald">Button</button>
+        </div>
+
+      </section>
     );
   }
 }
