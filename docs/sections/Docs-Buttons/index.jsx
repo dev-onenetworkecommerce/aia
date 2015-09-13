7d18dc6e4331723a9f5791591fff49deac38f390
maebe22 <maeby1115@gmail.com>
2015-09-13 11:29:37 +0800

    added inverted and contextual classes

--- docs/sections/Docs-Buttons/index.jsx
+++ docs/sections/Docs-Buttons/index.jsx
@@ -3,25 +3,69 @@ import React from 'react';
 export default class Buttons extends React.Component {
   render() {
     return (
-      <div>
-        <section className="doc-bottom-space-large">
-          <h2>Light Background</h2>
-            <div className="doc-bottom-space">
-              <h4>Normal</h4>          
-              <button className="btn">Button</button>
-            </div>
+      <div className="row">
+          <div className="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+            <h2>Light Background</h2>
+              <div className="doc-bottom-space">
+                <h4>Normal</h4>          
+                <button className="btn">Button</button>
+              </div>
 
-            <div className="doc-bottom-space">            
-              <h4>Disabled</h4>
-              <button className="btn -disabled">Disabled</button>
-            </div>
-        </section>
-
-        <section className="doc-bottom-space-large">
-          <h2>Button Group</h2>
-          <div>
+              <div className="doc-bottom-space">            
+                <h4>Disabled</h4>
+                <button className="btn -disabled">Disabled</button>
+              </div>
           </div>
-        </section>
+
+          <div className="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+            <h2>Inverted Button</h2>
+            <div className="doc-bottom-space">
+              <h4>Normal</h4>
+                <div className="doc-bottom-space">
+                  <button className="btn -inverted">Button</button>
+                </div>
+
+                <div className="doc-bottom-space">
+                  <button className="btn -inverted -crimson">Button</button>
+                </div>
+
+                <div className="doc-bottom-space">
+                  <button className="btn -inverted -sky">Button</button>
+                </div>
+
+                <div className="doc-bottom-space">
+                  <button className="btn -inverted -emerald">Button</button>
+                </div>
+
+                <div className="doc-bottom-space">
+                  <button className="btn -inverted -sun">Button</button>
+                </div>
+            </div>
+          </div>
+
+          <div className="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+            <h2>Color</h2>
+
+            <div className="doc-bottom-space">
+              <button className="btn -default">Button</button>
+            </div>
+
+            <div className="doc-bottom-space">
+              <button className="btn -sky">Button</button>
+            </div>
+
+            <div className="doc-bottom-space">
+              <button className="btn -emerald">Button</button>
+            </div>
+
+            <div className="doc-bottom-space">
+              <button className="btn -sun">Button</button>
+            </div>
+
+            <div className="doc-bottom-space">
+              <button className="btn -crimson">Button</button>
+            </div>
+          </div>
       </div>
     );
   }
