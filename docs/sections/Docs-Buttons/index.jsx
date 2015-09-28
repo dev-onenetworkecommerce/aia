c0686d15ffefe6e7ad5f3079accaab5788feb9b0
srph <seraphipod@gmail.com>
2015-09-28 09:39:02 +0800

    [Docs] Fix input groups

--- docs/sections/Docs-Buttons/index.jsx
+++ docs/sections/Docs-Buttons/index.jsx
@@ -11,30 +11,36 @@ export default class Buttons extends React.Component {
 
         <section className="doc-bottom-space-large">
           <h3 className="doc-heading">Variants</h3>
-          <button className="btn -primary">Primary</button>
-          <button className="btn -sky">Sky</button>
-          <button className="btn -emerald">Emerald</button>
-          <button className="btn -sun">Sun</button>
-          <button className="btn -crimson">Crimson</button>
+          <div className="doc-btn-group">
+            <button className="btn -primary">Primary</button>
+            <button className="btn -sky">Sky</button>
+            <button className="btn -emerald">Emerald</button>
+            <button className="btn -sun">Sun</button>
+            <button className="btn -crimson">Crimson</button>
+          </div>
         </section>
 
         <section className="doc-bottom-space-large">
           <h3 className="doc-heading">Sizes</h3>
           <p>Apply one of .-xs, .-small, .-large</p>
-          <button className="btn -xs">Extra Small</button>
-          <button className="btn -small">Small</button>
-          <button className="btn">Default</button>
-          <button className="btn -large">Large</button>
+          <div className="doc-btn-group">
+            <button className="btn -xs">Extra Small</button>
+            <button className="btn -small">Small</button>
+            <button className="btn">Default</button>
+            <button className="btn -large">Large</button>
+          </div>
         </section>
 
         <section className="doc-bottom-space-large">
           <h3 className="doc-heading">Inverted</h3>
-          <button className="btn -inverted -default">Button</button>
-          <button className="btn -inverted -primary">Button</button>
-          <button className="btn -inverted -sky">Button</button>
-          <button className="btn -inverted -sun">Button</button>
-          <button className="btn -inverted -crimson">Button</button>
-          <button className="btn -inverted -emerald">Button</button>
+          <div className="doc-btn-group">
+            <button className="btn -inverted -default">Button</button>
+            <button className="btn -inverted -primary">Button</button>
+            <button className="btn -inverted -sky">Button</button>
+            <button className="btn -inverted -sun">Button</button>
+            <button className="btn -inverted -crimson">Button</button>
+            <button className="btn -inverted -emerald">Button</button>
+          </div>
         </section>
 
         <section className="doc-bottom-space-large">
@@ -46,11 +52,13 @@ export default class Buttons extends React.Component {
 
         <section className="doc-bottom-space-large">
           <h3 className="doc-heading">Rounded Button</h3>
-          <button className="btn -default -rounded">Button</button>
-          <button className="btn -sky -rounded">Button</button>
-          <button className="btn -emerald -rounded">Button</button>
-          <button className="btn -sun -rounded">Button</button>
-          <button className="btn -crimson -rounded">Button</button>
+          <div className="doc-btn-group">
+            <button className="btn -default -rounded">Button</button>
+            <button className="btn -sky -rounded">Button</button>
+            <button className="btn -emerald -rounded">Button</button>
+            <button className="btn -sun -rounded">Button</button>
+            <button className="btn -crimson -rounded">Button</button>
+          </div>
         </section>
 
         <section className="doc-bottom-space-large">
