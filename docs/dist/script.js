0353df54e7191fe44d3d7c72fd2b044c890f164d
srph <seraphipod@gmail.com>
2015-12-09 15:31:21 +0800

    Fix drop-shadow rules.
    - Supply proper arguments.
    - Tweak arguments by a small value.
    - Fix syntax.

--- docs/dist/script.js
+++ docs/dist/script.js
@@ -25555,7 +25555,7 @@
 
 	module.exports = {
 		"name": "aia",
-		"version": "0.6.2",
+		"version": "0.7.0",
 		"description": "The UI Toolkit that powers ONE's internal projects",
 		"main": "lib/index",
 		"scripts": {
