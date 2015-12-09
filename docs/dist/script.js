f1d247123dfb064bbb1655c94a58b02e3350afc7
srph <seraphipod@gmail.com>
2015-12-09 15:33:07 +0800

    Aia 0.7.1

--- docs/dist/script.js
+++ docs/dist/script.js
@@ -25555,7 +25555,7 @@
 
 	module.exports = {
 		"name": "aia",
-		"version": "0.7.0",
+		"version": "0.7.1",
 		"description": "The UI Toolkit that powers ONE's internal projects",
 		"main": "lib/index",
 		"scripts": {
